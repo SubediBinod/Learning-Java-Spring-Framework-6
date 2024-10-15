f24b66e34c65bc044e9a5cadcf3eec98b28b680b
Binod <bidhaksubedi77@gmail.com>
2024-10-15 21:58:46 +0530

    Spring SEcurity

---
+++ spring-security/src/main/java/com/binod/spring_security/service/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.binod.spring_security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.binod.spring_security.dao.UserRepo;
+import com.binod.spring_security.model.User;
+import com.binod.spring_security.model.UserPrincipal;
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepo repo;
+	
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+	User user= repo.findByUsername(username);
+	
+	if (user==null) {
+		System.out.println("User 404");
+		throw new UsernameNotFoundException("User 404");
+	}
+		 return new UserPrincipal(user);
+	}
+
+}
